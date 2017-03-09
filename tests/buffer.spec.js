d2b790210c120475688668ea2e5e629505c8bfc2
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-03-10 01:55:39 +0300

    implemented `buffer`

--- tests/buffer.spec.js
+++ tests/buffer.spec.js
@@ -3,14 +3,46 @@
 describe('Buffer', function () {
   chai.use(chaiDiff);
 
-  it('one level indent', function () {
+  var appendObject = function (buffer) {
+    buffer.push('{');
+    buffer.indent();
+    buffer.append('"key": "value"');
+    buffer.unindent();
+    buffer.append('}');
+    return buffer;
+  };
+
+  var fillBuffer = function (buffer, values) {
+    buffer.push('{');
+    buffer.indent();
+    values.forEach(function (it) {
+      buffer.append(it);
+    });
+    buffer.unindent();
+    buffer.append('}');
+    return buffer;
+  };
+
+  it('one level indent single field', function () {
+    var buffer = appendObject(new window.Buffer());
+
+    expect(buffer.print()).to.equal(JSON.stringify({key: 'value'}, void 0, 2));
+  });
+
+  it('one level indent multi-field', function () {
+    var buffer = fillBuffer(new window.Buffer(), ['"key1": 1']);
+
+    expect(buffer.print()).to.equal(JSON.stringify({key1: 1}, void 0, 2));
+  });
+
+  it('two level indent', function () {
     var buffer = new window.Buffer();
     buffer.push('{');
     buffer.indent();
-    buffer.push('key: value');
+    buffer.append('"key": ' + appendObject(buffer.newBuffer()).print());
     buffer.unindent();
-    buffer.push('}');
+    buffer.append('}');
 
-    expect(buffer.print()).to.equal(JSON.stringify({key: 'value'}, void 0, 2));
+    expect(buffer.print()).to.equal(JSON.stringify({key: {key: 'value'}}, void 0, 2));
   });
 });
