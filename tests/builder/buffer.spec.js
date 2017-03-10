9159e5c24b095176cdf4753f8862168eb61ac405
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-03-11 02:11:42 +0300

    fixed array cleaned code

--- tests/builder/buffer.spec.js
+++ tests/builder/buffer.spec.js
@@ -3,37 +3,16 @@
 describe('Buffer', function () {
 
   it('push', function () {
-    var buffer = new window.Buffer();
+    var buffer = new window.StreamBuffer();
 
     expect(buffer.push('{}').print()).to.equal('{}');
   });
 
   it('print with indent', function () {
-    var buffer = new window.Buffer();
-    expect(buffer.push((new window.Buffer()).outerIndent().push('{}')).print()).to.equal('  {}');
-  });
-
-  it('indent print', function () {
-
-    var print = function (buffer, level) {
-      level = level || 0;
-      buffer = buffer || new window.Buffer();
-      if (level > 2) {
-        return buffer.push('{}');
-      }
-      buffer.push('{').newline().outerIndent().indent().push(print(null, level + 1)).newline().outerIndent().push('}');
-      return buffer;
-    };
-
-
-    expect(print().print()).to.equal(
-        '{\n' +
-      '  {\n' +
-      '    {\n' +
-      '      {}\n' +
-      '    }\n' +
-      '  }\n' +
-      '}');
+    var buffer = new window.StreamBuffer();
+    expect(buffer.push('{').
+      shift().indent().push('{}').indent().unshift().
+      push('}').print()).to.equal('{  {}  }');
   });
 
   it('stream print', function () {
