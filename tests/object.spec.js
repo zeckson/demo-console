4bc2ced6b7ec15d612a24a30ec2a281a1cb6f456
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-03-10 18:21:48 +0300

    cleanup tests

--- tests/object.spec.js
+++ tests/object.spec.js
@@ -1,44 +1,30 @@
 'use strict';
 
 describe('Object builder', function () {
-  var appendObject = function (buffer) {
-    return fillBuffer(buffer, [['"key"', '"value"']]);
-  };
-
-  var fillBuffer = function (buffer, values) {
-    var builder = new window.ObjectBuilder(buffer);
-    builder.begin();
-    values.forEach(function (it) {
-      builder.add(it[0], it[1]);
-    });
-    builder.end();
-    return buffer;
-  };
-
   var object = function () {
     return new window.ObjectBuilder(new window.Buffer());
   };
 
   describe('simple tests', function () {
     it('empty', function () {
-      var buffer = new window.Buffer();
-      var builder = new window.ObjectBuilder(buffer);
-      builder.begin();
-      builder.end();
+      var builder = object().begin().end();
 
-      expect(buffer.print()).to.equal(JSON.stringify({}, void 0, 2));
+      expect(builder.buffer.print()).to.equal(JSON.stringify({}, void 0, 2));
     });
 
     it('single value', function () {
-      var buffer = appendObject(new window.Buffer());
+      var builder = object().begin().add('"key"', '"value"').end();
 
-      expect(buffer.print()).to.equal(JSON.stringify({key: 'value'}, void 0, 2));
+      expect(builder.buffer.print()).to.equal(JSON.stringify({key: 'value'}, void 0, 2));
     });
 
     it('multi value', function () {
-      var buffer = fillBuffer(new window.Buffer(), [['"key1"', '1'], ['"key2"', '2']]);
+      var builder = object().begin().
+        add('"key"', '"value"').
+        add('"key2"', '"value"').
+        end();
 
-      expect(buffer.print()).to.equal(JSON.stringify({key1: 1, key2: 2}, void 0, 2));
+      expect(builder.buffer.print()).to.equal(JSON.stringify({key: 'value', key2: 'value'}, void 0, 2));
     });
   });
 
