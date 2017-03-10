af0336cb3b299ada1a85385d68cc0aa4597b455b
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-03-11 00:52:33 +0300

    implemented StreamBuffer

--- tests/builder/buffer.spec.js
+++ tests/builder/buffer.spec.js
@@ -13,4 +13,50 @@ describe('Buffer', function () {
     expect(buffer.push((new window.Buffer()).outerIndent().push('{}')).print()).to.equal('  {}');
   });
 
+  it('indent print', function () {
+
+    var print = function (buffer, level) {
+      level = level || 0;
+      buffer = buffer || new window.Buffer();
+      if (level > 2) {
+        return buffer.push('{}');
+      }
+      buffer.push('{').newline().outerIndent().indent().push(print(null, level + 1)).newline().outerIndent().push('}');
+      return buffer;
+    };
+
+
+    expect(print().print()).to.equal(
+      '{\n' +
+      '  {\n' +
+      '    {\n' +
+      '      {}\n' +
+      '    }\n' +
+      '  }\n' +
+      '}');
+  });
+
+  it('stream print', function () {
+
+    var print = function (buffer, level) {
+      level = level || 0;
+      buffer = buffer || new window.StreamBuffer();
+      if (level > 2) {
+        return buffer.push('{}');
+      }
+      buffer.push('{').newline().shift().indent().push(print(buffer, level + 1)).newline().unshift().indent().push('}');
+      return buffer;
+    };
+
+
+    expect(print().print()).to.equal(
+      '{\n' +
+      '  {\n' +
+      '    {\n' +
+      '      {}\n' +
+      '    }\n' +
+      '  }\n' +
+      '}');
+  });
+
 });
